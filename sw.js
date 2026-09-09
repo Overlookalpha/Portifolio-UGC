750874150462a5335e22dc35280e9b1e7459ded1
Overlookalpha <isaiasfr26@gmail.com>
2026-09-09 11:41:59 +0100

    Atualizar cache do service worker (v2)

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   ============================================================================
 */
 
-var CACHE_VERSAO = "v1";
+var CACHE_VERSAO = "v2";
 var CACHE_ESSENCIAL = "ugc-portfolio-essencial-" + CACHE_VERSAO;
 var CACHE_RUNTIME = "ugc-portfolio-runtime-" + CACHE_VERSAO;
 
